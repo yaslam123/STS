861116eea696c3c439ccf3d1b8471699f59e07e4
yaslam123 <yaslamahmed52@gmail.com>
2024-08-24 11:50:09 +0300

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d926c46bdd1ea51c"; }
+/******/ 		__webpack_require__.h = function() { return "59de2a69d7b4fc9e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
